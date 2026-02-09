7ff18c42dd376a3b45ae432b2dcc9de0cff7a4f3
Saad Madni <saadmadni84@gmail.com>
2026-02-09 22:35:32 +0530

    "c"

--- BootP02/src/main/java/com/cfs/bootp02/controller/StudentController.java
+++ BootP02/src/main/java/com/cfs/bootp02/controller/StudentController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome student......";
+        return "Welcome student..";
     }
 }
